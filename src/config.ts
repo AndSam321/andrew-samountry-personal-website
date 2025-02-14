aca3e530a46a36b16db6d071309f28cfd0be4185
Andrew Samountry <andsam321@gmail.com>
2025-02-13 21:36:31 -0600

    Added ThunderThrift to projects

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
-export const GENERATE_SLUG_FROM_TITLE = true
-export const TRANSITION_API = true
+export const SITE_TITLE = "Andrew Samountry | Software Engineer and Student";
+export const SITE_DESCRIPTION =
+  "Personal portfolio and blog of Andrew Samountry, showcasing software engineering projects and technical expertise.";
+export const SITE_IMAGE = "/profile2.webp";
+export const GENERATE_SLUG_FROM_TITLE = true;
+export const TRANSITION_API = true;
